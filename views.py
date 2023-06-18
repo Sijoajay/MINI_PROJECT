4b7997e178c175300c86aea8c2c7752e03496e00
preethi manoj <115825415+Preitzz@users.noreply.github.com>
2023-06-18 20:18:32 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the name of your home page URL pattern
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
